b3f81645cdbc964cc326fa84927505c3ad3fcad4
李仲登 <harden.li@ifastfinancial.com.cn>
2023-08-20 18:59:44 +0800

    add sign out button on MessageList

--- src/message/screen/MessageList.tsx
+++ src/message/screen/MessageList.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import {useNavigation} from '@react-navigation/native';
+import React, {useCallback, useEffect} from 'react';
 import {View, StyleSheet} from 'react-native';
 import Example from '../../example';
+import {GlogalScreenNavigationProps} from '../../navigation/types';
+import {useAuthStore} from '../../store';
+import SignOutButton from '../../example/SignOutButton';
 
 export type MessageListRouteParams = undefined;
 
 export default () => {
+  const navigation =
+    useNavigation<GlogalScreenNavigationProps<'MessageList'>>();
+  const token = useAuthStore(state => state.token);
+  const headerLeft = useCallback(
+    () => (token ? <SignOutButton /> : null),
+    [token],
+  );
+  useEffect(() => {
+    navigation.setOptions({headerLeft});
+  }, [navigation, headerLeft]);
   return (
     <View style={styles.container}>
       <Example />
